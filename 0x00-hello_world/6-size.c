c3f9a6d92513e2c8d7cd5f77ffcd08d93395a7b5
Esenu David <esenudavid1@gmail.com>
2023-03-16 07:48:28 -0700

    First C complete

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non zero otherwise
+ */
+int main(void)
+{
+	printf("Size of char: %1d byte(s)\n", sizeof(char));
+	printf("Size of int: %1d byte(s)\n", sizeof(int));
+	printf("Size of long int: %1d byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %1d byte(s)\n", sizeof(long long));
+	printf("size of float: %1d byte(s)\n", sizeof(float));
+	return(0);
+}
