c02ee70c73b41b0e120e853f921652a96445063d
Esenu David <esenudavid1@gmail.com>
2023-03-16 07:52:47 -0700

    new changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ int main(void)
 	printf("Size of int: %1d byte(s)\n", sizeof(int));
 	printf("Size of long int: %1d byte(s)\n", sizeof(long int));
 	printf("Size of long long int: %1d byte(s)\n", sizeof(long long));
-	printf("size of float: %1d byte(s)\n", sizeof(float));
+	 printf("Size of float: %1d byte(s)\n", sizeof(float));
 	return(0);
 }
