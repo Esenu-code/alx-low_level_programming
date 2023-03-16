48186766b09b937d3ced9c02f89615ca26e346a9
Esenu David <esenudavid1@gmail.com>
2023-03-16 08:27:28 -0700

    finale

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - prints progamming is like building a mutlilingual puzzle
+* main - prints programming is like building a mutlilingual puzzle
 * Return: 0 if exited properly, non zero otherwise
 */
 int main(void)
 {
-	puts("\"Progamming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
